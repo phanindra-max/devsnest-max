f48a5b70e1bcefd99f46b435f3689fd09e3eae68
Kalaga Satya Phanindra Kumar <phanindra.1756.kkr@gmail.com>
2021-07-27 20:08:08 +0530

    THA2

--- THA2/productOfArray.py
+++ THA2/productOfArray.py
@@ -1,4 +1,4 @@
 from numpy import flip, cumprod
 class Solution:
     def productExceptSelf(self, nums):
-	return cumprod([1] + nums[:-1]) * flip(cumprod(flip(nums[1:] + [1])))
+	    return cumprod([1] + nums[:-1]) * flip(cumprod(flip(nums[1:] + [1])))
